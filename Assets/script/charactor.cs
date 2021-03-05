a49284698420f9baa6b2bf246828ed001a612b7b
kjw1749 <80082529+kjw1749@users.noreply.github.com>
2021-03-05 12:05:52 +0900

    .

---
+++ Assets/script/charactor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+    public void GetInfo()
+    {
+        Debug.Log("이름 : " + name);
+        Debug.Log("레벨 : " + level);
+    }
+
+    public void LevelUp()
+    {
+        this.level++;
+        Debug.Log("레벨 업~!");
+    }
+
+}
+
+
+public class charactor : MonoBehaviour
+{
+    void Start()
+    {
+        Player myPlayer = new Player("김지웅");
+        myPlayer.GetInfo();
+        myPlayer.LevelUp();
+        myPlayer.GetInfo();
+    }
+
+}
